29b02f9cff6cc28934dff6e1fee7dc502f68caec
martinx22 <112469065+martinx22@users.noreply.github.com>
2025-06-01 16:09:28 +0700

    main.py

--- main.py
+++ main.py
@@ -69,5 +69,9 @@ def webhook():
 def home():
     return "星辰已上线 ✨"
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
